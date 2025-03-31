6558a12fe3183875bd794a4d9ae9c0aaf50ef6f7
wilkenio <wilkeniopereiraguitarra@gmail.com>
2025-03-31 20:47:39 -0300

    📦 NEW: implementação do Swagger

--- api/src/main/java/com/financeiro/api/infra/security/SecurityConfig.java
+++ api/src/main/java/com/financeiro/api/infra/security/SecurityConfig.java
@@ -26,8 +26,10 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        // 🔥 Libera acesso ao Swagger
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
