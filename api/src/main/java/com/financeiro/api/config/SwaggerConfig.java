3dd9f57f77026e01e0b9d13329518d888319d9a3
wilkenio <wilkeniopereiraguitarra@gmail.com>
2025-04-02 18:30:04 -0300

    📦 NEW: correção do CORSe Implementação do cookie

--- api/src/main/java/com/financeiro/api/config/SwaggerConfig.java
+++ api/src/main/java/com/financeiro/api/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.financeiro.api.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +13,10 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API Financeiro")
+                        .title("API Squad 25")
                         .version("1.0")
-                        .description("Documentação da API Financeira"));
+                        .description("Documentação do Sistema Financeiro do Squad 25"))
+                .addServersItem(new Server().url("http://localhost:8080").description("Servidor Local"))
+                .addServersItem(new Server().url("https://apisquad25.fourdevs.com.br").description("Servidor de Produção"));
     }
 }
