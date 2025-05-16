a47758842b7e6c4c02139e959d4ec136c0566fac
denis <dennisjsilvaa@gmail.com>
2025-05-16 18:58:41 -0300

    👌 IMPROVE: Adição de funcionalidades a transação

---
+++ api/src/main/java/com/financeiro/api/domain/enums/Periodicity.java
@@ -0,0 +1,12 @@
+package com.financeiro.api.domain.enums;
+
+public enum Periodicity {
+    DIARIO,
+    SEMANAL,
+    QUINZENAL,
+    MENSAL,
+    TRIMESTRAL,
+    SEMESTRAL,
+    ANUAL
+}
+
--- api/src/main/java/com/financeiro/api/dto/transactionDTO/TransactionResponseDTO.java
+++ api/src/main/java/com/financeiro/api/dto/transactionDTO/TransactionResponseDTO.java
@@ -4,25 +4,29 @@ import com.financeiro.api.domain.Account;
 import com.financeiro.api.domain.Category;
 import com.financeiro.api.domain.Subcategory;
 import com.financeiro.api.domain.enums.*;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record TransactionResponseDTO(
-                Account account,
-                Category category,
-                Subcategory subcategory,
-                UUID id,
-                String name,
-                TransactionType type,
-                Status status,
-                LocalDateTime releaseDate,
-                Double value,
-                String description,
-                TransactionState state,
-                String additionalInformation,
-                Frequency frequency,
-                Integer installments,
-                LocalDateTime createdAt,
-                LocalDateTime updatedAt
-) {
-}
+        Account account,
+        Category category,
+        Subcategory subcategory,
+        UUID id,
+        String name,
+        TransactionType type,
+        Status status,
+        LocalDateTime releaseDate,
+        Double value,
+        String description,
+        TransactionState state,
+        String additionalInformation,
+        Frequency frequency,
+        Integer installments,
+        Periodicity periodicity,       
+        Boolean businessDayOnly,       
+        Integer installmentNumber,     
+        UUID recurringGroupId,         
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {}
